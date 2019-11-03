78b39fdcd81e1071c6c42a2a4a3f1bd9094b9a09
Hyun Jae Cho <hyunjaecho1213@gmail.com>
2019-11-03 16:24:01 -0500

    added script to perturb multiple .bed files easily

---
+++ bedshift/bedscript.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Change NUM_BEDFILES, BEDFILE, and OUTPUT_FILE.
+# Other parameters are set to the default values. Edit as needed.
+# To run, execute ./bedscript.sh in Terminal.
+
+NUM_BEDFILES=NUMBER
+for (( c=1; c<=$NUM_BEDFILES; c++ ))
+do
+	BEDFILE=PATH/TO/ORIGINAL/FILE.bed
+	DROP_RATE=0.0
+
+	ADD_RATE=0.0
+	ADD_MEAN=320.0
+	ADD_STDEV=30.0
+
+	SHIFT_RATE=0.05
+	SHIFT_MEAN=0.0
+	SHIFT_STDEV=50.0
+
+	CUT_RATE=0.0
+	MERGE_RATE=0.0
+	OUTPUT_FILE=PATH/TO/PERTURBED/FILE.bed
+
+	bedshift --bedfile $BEDFILE --droprate $DROP_RATE --addrate $ADD_RATE --addmean $ADD_MEAN --addstdev $ADD_STDEV --shiftrate $SHIFT_RATE --shiftmean $SHIFT_MEAN --shiftstdev $SHIFT_STDEV --cutrate $CUT_RATE --mergerate $MERGE_RATE --outputfile $OUTPUT_FILE
+done
