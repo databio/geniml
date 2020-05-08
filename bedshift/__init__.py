a1ac18d95d3f3dff80bd7b02c44a60d847f59a78
nsheff <nsheff@users.noreply.github.com>
2020-05-08 17:03:46 +0000

    comply with lucidoc

--- bedshift/__init__.py
+++ bedshift/__init__.py
@@ -1,9 +1,10 @@
 # Project configuration, particularly for logging.
 
 import logmuse
+from .bedshift import Bedshift
 from ._version import __version__
 
-__classes__ = []
+__classes__ = ["Bedshift"]
 __all__ = __classes__ + []
 
 logmuse.init_logger("bedshift")
