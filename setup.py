ee0b9428744b63f73a7c0c300d6bdd507a9ea692
aaron-gu <ag5ym@virginia.edu>
2020-05-20 13:24:32 -0400

    improve usability of python API

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Topic :: System :: Distributed Computing"
