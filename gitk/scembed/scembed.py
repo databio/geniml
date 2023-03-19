59dce47512ef6936d8da4db44b048633cae17fed
Nathan LeRoy <NLeRoy917@gmail.com>
2023-03-19 14:28:14 -0400

    clean up API

--- gitk/scembed/scembed.py
+++ gitk/scembed/scembed.py
@@ -5,11 +5,7 @@ from typing import Dict, Iterable, List
 from six.moves import cPickle as pickle  # for performance
 from gensim.models import Word2Vec
 from numba import config
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-# Ensure pyqt5 is upgraded (pip install --user --upgrade pyqt5
-# import csv
 import numpy as np
 import pandas as pd
 import os
@@ -25,34 +21,6 @@ _LOGGER = getLogger(PKG_NAME)
 
 # set the threading layer before any parallel target compilation
 config.THREADING_LAYER = "threadsafe"
-
-
-def embedding_avg(model, document):
-    _LOGGER.debug(f"embedding_avg model: {model}")
-    listOfWVs = []
-    if type(document) is list:
-        _LOGGER.debug(f"embedding_avg document is a list")
-        for word in document:
-            if word in model.wv.vocab:
-                listOfWVs.append(model[word])
-        if len(listOfWVs) == 0:
-            return np.zeros([len(model[list(model.wv.vocab.keys())[0]])])
-    else:
-        _LOGGER.debug(f"embedding_avg document is NOT a list")
-        for word in document.split(" "):
-            if word in model.wv.vocab:
-                listOfWVs.append(model[word])
-        if len(listOfWVs) == 0:
-            return np.zeros([len(model[list(model.wv.vocab.keys())[0]])])
-    return np.mean(listOfWVs, axis=0)
-
-
-def document_embedding_avg(document_Embedding, model):
-    document_Embedding_avg = {}
-    for file, doc in document_Embedding.items():
-        document_Embedding_avg[file] = embedding_avg(model, doc)
-    return document_Embedding_avg
-
 
 # shuffle the document to generate data for word2vec
 def shuffle_documents(
@@ -74,7 +42,7 @@ def shuffle_documents(
     return training_samples
 
 
-def train_Word2Vec(
+def train(
     documents: Iterable[Iterable[str]],
     window_size: int = 100,
     dim: int = 128,
@@ -119,97 +87,6 @@ def label_preprocessing(y: List[str], delim: str) -> List[str]:
     for y1 in y:
         y_cell.append(y1.split(delim)[0])
     return y_cell
-
-
-# This function reduces the dimension using umap and plot
-def UMAP_plot(data_X, y, title, nn, filename, umet, rasterize=False):
-    import umap
-    np.random.seed(42)
-    # TODO: make low_memory a tool argument
-    ump = umap.UMAP(
-        a=None,
-        angular_rp_forest=False,
-        b=None,
-        force_approximation_algorithm=False,
-        init="spectral",
-        learning_rate=1.0,
-        local_connectivity=1.0,
-        low_memory=False,
-        metric=umet,
-        metric_kwds=None,
-        min_dist=0.1,
-        n_components=2,
-        n_epochs=1000,
-        n_neighbors=nn,
-        negative_sample_rate=5,
-        output_metric=umet,
-        output_metric_kwds=None,
-        random_state=42,
-        repulsion_strength=1.0,
-        set_op_mix_ratio=1.0,
-        spread=1.0,
-        target_metric="categorical",
-        target_metric_kwds=None,
-        target_n_neighbors=-1,
-        target_weight=0.5,
-        transform_queue_size=4.0,
-        transform_seed=42,
-        unique=False,
-        verbose=False,
-    )
-    _LOGGER.info(f"-- Fitting UMAP data --")
-    # For UMAP,`pip install --user --upgrade pynndescent` for large data
-    ump.fit(data_X)
-    ump_data = pd.DataFrame(ump.transform(data_X))
-    ump_data = pd.DataFrame({"UMAP 1": ump_data[0], "UMAP 2": ump_data[1], title: y})
-    ump_data.to_csv(filename, index=False)
-    _LOGGER.info(f"-- Saved UMAP data as {filename} --")
-    fig, ax = plt.subplots(figsize=(8, 6.4))
-    plt.rc("font", size=11)
-    plate = sns.color_palette("husl", n_colors=len(set(y)))
-    sns.scatterplot(
-        x="UMAP 1",
-        y="UMAP 2",
-        hue=title,
-        s=10,
-        ax=ax,
-        palette=plate,
-        sizes=(10, 40),
-        data=ump_data,  # .sort_values(by = title),
-        rasterized=rasterize,
-    )
-    # TODO: only label a subset of the samples...
-    # bbox_to_anchor(xpos, ypos)
-    # See: https://stackoverflow.com/questions/30413789/matplotlib-automatic-legend-outside-plot
-    lgd = plt.legend(
-        bbox_to_anchor=(1.01, 1),
-        loc="upper left",
-        fontsize=11,
-        markerscale=2,
-        edgecolor="black",
-    )
-    plt.gcf().canvas.draw()
-    # Then define the transformation to go from pixel coordinates to Figure coordinates:
-    invFigure = plt.gcf().transFigure.inverted()
-    # Next, get the legend extents in pixels and convert to Figure coordinates.
-    # Pull out the farthest extent in the x direction since that is the canvas
-    # direction we need to adjust:
-    lgd_pos = lgd.get_window_extent()
-    lgd_coord = invFigure.transform(lgd_pos)
-    lgd_xmax = lgd_coord[1, 0]
-    # Do the same for the Axes:
-    ax_pos = plt.gca().get_window_extent()
-    ax_coord = invFigure.transform(ax_pos)
-    ax_xmax = ax_coord[1, 0]
-    # Finally, adjust the Figure canvas using tight_layout for the proportion
-    # of the Axes that must move over to allow room for the legend to fit
-    # within the canvas:
-    shift = 1 - (lgd_xmax - ax_xmax)
-    plt.gcf().tight_layout(rect=(0, 0, shift, 1))
-    # plt.legend(bbox_to_anchor=(1.1,1), loc="upper right", fontsize =  11,
-    #           markerscale=2, edgecolor = 'black')
-    return fig
-
 
 def save_dict(di_: dict, filename_: str):
     """
