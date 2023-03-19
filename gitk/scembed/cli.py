22525dfd83be4036decb78e6e0b53f177bc09c7f
Nathan LeRoy <NLeRoy917@gmail.com>
2023-03-19 14:33:43 -0400

    remove unnecessary plotting code

--- gitk/scembed/cli.py
+++ gitk/scembed/cli.py
@@ -9,12 +9,10 @@ from ._version import __version__
 from .const import *
 from .scembed import (
     convert_anndata_to_documents,
-    document_embedding_avg,
     load_scanpy_data,
     shuffle_documents,
-    train_Word2Vec,
+    train,
     label_preprocessing,
-    UMAP_plot,
     save_dict,
     load_dict,
 )
@@ -23,7 +21,7 @@ from .scembed import (
 def main():
     """MAIN"""
     parser = logmuse.add_logging_options(build_argparser())
-    args, remaining_args = parser.parse_known_args()
+    args, _ = parser.parse_known_args()
 
     if args.input is None:
         parser.print_help(sys.stderr)
@@ -58,7 +56,7 @@ def main():
         # load AnnData object
         sc_data = load_scanpy_data(args.input)
 
-        # re-create documents dictionary
+        # re-create documents dictionary from AnnData Object
         documents = convert_anndata_to_documents(sc_data)
 
         docs_filename = os.path.join(args.output, args.title + "_documents.pkl")
@@ -79,7 +77,7 @@ def main():
         _LOGGER.info(f"Shuffling documents")
         shuffeled_documents = shuffle_documents(documents, int(args.shuffle_repeat))
         _LOGGER.info(f"Constructing model")
-        model = train_Word2Vec(
+        model = train(
             shuffeled_documents,
             window_size=int(args.window_size),
             dim=int(args.dimension),
@@ -90,47 +88,4 @@ def main():
         _LOGGER.info(f"Model saved as: {model_filename}")
 
     _LOGGER.info(f"Number of words in w2v model: {len(model.wv.vocab)}")
-
-    _LOGGER.info(f"Calculate document embeddings.")
-    if not args.embed_file:
-        embeddings = document_embedding_avg(documents, model)
-        embeddings_dictfile = os.path.join(args.output, args.title + "_embeddings.pkl")
-        save_dict(embeddings, embeddings_dictfile)
-        embeddings_csvfile = os.path.join(args.output, args.title + "_embeddings.csv")
-        (
-            pd.DataFrame.from_dict(data=embeddings, orient="index").to_csv(
-                embeddings_csvfile, header=False
-            )
-        )
-        _LOGGER.info(f"Embeddings file saved as {embeddings_csvfile}")
-    else:
-        embeddings = load_dict(args.embed_file)
-        _LOGGER.info(f"Loaded {args.embed_file}")
-
-    x = pd.DataFrame(embeddings).values
-    y = list(embeddings.keys())
-    y = label_preprocessing(y, args.label_delimiter)
-
-    _LOGGER.info(f"Generating plot.")
-    coordinates_csvfile = os.path.join(args.output, args.title + "_xy_coords.csv")
-    plot_name = (
-        f"_nocells{args.nocells}_noreads{args.noreads}_"
-        f"dim{args.dimension}_win{args.window_size}_"
-        f"mincount{args.min_count}_"
-        f"shuffle{args.shuffle_repeat}_"
-        f"umap_nneighbours{args.umap_nneighbours}_"
-        f"umap-metric{args.umap_metric}.svg"
-    )
-    plot_filename = os.path.join(args.output, "figs", plot_name)
-    fig = UMAP_plot(
-        x.T,
-        y,
-        args.title,
-        int(args.umap_nneighbours),
-        coordinates_csvfile,
-        args.umap_metric,
-        args.rasterize,
-    )
-    _LOGGER.info(f"Saving UMAP plot")
-    fig.savefig(plot_filename, format="svg")
     _LOGGER.info(f"Pipeline Complete!")
