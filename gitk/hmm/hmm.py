4110d29ad581a685a7c87d8413b4e2bdb0ec85f8
Julia820 <r.julia98@gmail.com>
2023-05-19 09:31:49 -0400

    fixed transition matrix

--- gitk/hmm/hmm.py
+++ gitk/hmm/hmm.py
@@ -83,8 +83,12 @@ def read_data(start, core, end, chrom, normalize=True):
 
 
 def find_full_full_pos(seq, gap_size=1000, area_size=500):
-    """Look for nonzero positions in coverage matrix,
-    when most of the positions are zero"""
+    """Look for nonzero positions in coverage matrix, when most of the positions are zero
+    :param ndarray seq: vector with information about non-zero positions
+    :param int gap_size: size of minium gap between non-zero positions that are separated
+    :param int area_size: size of the area around non-zero positions to be included in the result
+    :return list: list of starts of non-zero regions and list of ends of non-zero regions
+    """
     size = len(seq)
     seq = np.argwhere(seq >= 1).flatten()
     starts, ends = [], []
@@ -101,8 +105,12 @@ def find_full_full_pos(seq, gap_size=1000, area_size=500):
 
 
 def find_full_empty_pos(seq, gap_size=10000, area_size=1000):
-    """Look for nonzero positions in coverage matrix,
-    when most of the positions are nonzero"""
+    """Look for nonzero positions in coverage matrix, when most of the positions are nonzero
+    :param ndarray seq: vector with information about non-zero positions
+    :param int gap_size: size of minium gap between non-zero positions that are separated
+    :param int area_size: size of the area around non-zero positions to be included in the result
+    :return list: list of starts of non-zero regions and list of ends of non-zero regions
+    """
     size = len(seq)
     seq = np.argwhere(seq == 0).flatten()
     starts, ends = [], []
@@ -150,13 +158,12 @@ def ana_region(region, start_s):
 def predictions_to_bed(states, chrom, bedname, save_max_cove=False, cove_file=None):
     """
     Save HMM prediction into a file
-    :param array states: result of HMM prediction
+    :param ndarray states: result of HMM prediction
     :param str chrom: which chromosome is being analysed
     :param str bedname: path to the output file
-    :param bool save_max_cove: whether to save the maximum peak
-     coverage to output file, can result in nonstandard bed file
-    :param str cove_file: file with core coverage, require for
-     saving maximum peak coverage
+    :param bool save_max_cove: whether to save the maximum peak coverage to output
+     file, can result in nonstandard bed file
+    :param str cove_file: file with core coverage, require for saving maximum peak coverage
     """
     ind = np.argwhere(states != 3)
     ind = ind.flatten()
