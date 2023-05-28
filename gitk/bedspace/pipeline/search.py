6b17df5af43862d6386e5a4fcd804e7669fee492
“Erfaneh67” <erfaneh.gharavi@gmail.com>
2023-05-28 17:39:32 -0400

    Update search functions

--- gitk/bedspace/pipeline/search.py
+++ gitk/bedspace/pipeline/search.py
@@ -23,57 +23,20 @@ def run_scenario1(
 
     # PLACE SEARCH CODE HERE
     searchterm = query
-    
     distance = pd.read_csv(distances)
     distance.file_label = distance.file_label.str.lower()
     distance.search_term = distance.search_term.str.lower()
     distance = distance.drop_duplicates()
     search_table = pd.pivot_table(distance, values='score', index=['filename'],columns=['search_term']).reset_index()
-    search_table = pd.merge(distance[['filename','file_label']].drop_duplicates(), search_table, on = 'filename').drop_duplicates()
-    search_table= search_table.merge(meta_test, left_on='filename', right_on='file_name')
+    df = search_table[['filename', searchterm]].sort_values(by=[searchterm], ascending=False)[0:num_results]
+    df=df.sort_values(by=[searchterm], ascending=True)
+    df['color']='green'
+    plt= df.plot.barh(x='filename', y=searchterm, figsize=(6,4), fontsize=10, color=list(df['color']))
+    plt.set_xlabel('Similarity', fontsize=10)
+    plt.set_ylabel('File_name', fontsize=10)
+    plt.axis(xmin=0.0, xmax=1.01)
 
-    if(search=='cell'):
-        ind=0
-        training_labels = cell_types
-    else:
-        ind=1
-        training_labels=targets
-
-    training_labels=sorted(training_labels)
-    
-    ### ???
-    training_labels = list(search_table)[2:-4]
-
-    len_targets = len(search_table.file_label[0].split(' _'))-1
-
-    search_table.file_label = search_table.file_label.str.split(' _', expand=True)[np.min([ind, len_targets])]
-    
-
-    
-    search_table = search_table[['filename','file_label', 'original_label']+(training_labels)]
-    search_table['predicted_label'] = search_table[list(search_table)[3:]].idxmin(axis=1)
-    
-
-
-    nof=len(search_table[search_table.file_label.str.contains(searchterm)])
-    df=search_table[['filename', 'file_label', 'original_label', searchterm]].sort_values(by=[searchterm])[0:num_results]
-    df=df.sort_values(by=[searchterm], ascending=False)
-
-    df['color']='gray'
-    df.loc[df.file_label.str.contains(searchterm), 'color'] = 'green'
-    if(len(df[df.color=='green']) == nof):
-        df.loc[(df.color!='green'), 'color'] = 'gray'
-
-    df[searchterm] = 1 - df[searchterm]
-
-    plt= df.plot.barh(x='original_label', y=searchterm, figsize=(10,7), fontsize=16, color=list(df['color']))
-    plt.set_xlabel('Similarity', fontsize=15)
-    plt.set_ylabel('original_label', fontsize=15)
-
-    plt.axis(xmin=0.5, xmax=1.01)
-
-    plt.figure.savefig('{}/figures/S1/{}_nof{}.svg'.format(output, searchterm, nof), format = 'svg', bbox_inches='tight')
-        
+    plt.figure.savefig('{}/{}_nof{}.svg'.format(output, query, num_results), format = 'svg', bbox_inches='tight')
 
         
 
@@ -94,74 +57,24 @@ def run_scenario2(
 
     _LOGGER.info("Running search...")
 
-    file = query
     
     # PLACE SEARCH CODE HERE
+    file = query
     distance =pd.read_csv(distances)
     distance.file_label = distance.file_label.str.lower()
     distance.search_term = distance.search_term.str.lower()
     distance = distance.drop_duplicates()
-    search_table = pd.pivot_table(distance, values='score', index=['filename'],columns=['search_term']).reset_index()
-    search_table= search_table.merge(meta_test, left_on='filename', right_on='file_name')
-    search_table = pd.merge(distance[['filename','file_label']].drop_duplicates(), search_table, on = 'filename').drop_duplicates()
-
-    if('cell' in search):
-        ind=0
-        training_labels=cell_types
-    else:
-        ind=1
-        training_labels=targets
-
-    training_labels=sorted(training_labels)
 
 
-    len_targets = len(search_table.file_label[0].split(' _'))-1
+    df = distance[distance.filename == query].sort_values(by = ['score'], ascending = True)
 
-    search_table.file_label = search_table.file_label.str.split(' _', expand=True)[np.min([ind, len_targets])]
-    
-          
-        
-    search_table = search_table[['filename','file_label', 'original_label']+(training_labels)]
-    search_table['predicted_label'] = search_table[list(search_table)[3:]].idxmin(axis=1)
-
-    i=0
-    b=search_table
-    all_weights=[]
-    for fil in b.filename:
-        c=b[b.filename==fil]['file_label']
-        i=c.index[0]
-
-        a = b[b.filename==fil][list(b[b.filename==fil])[3:-1]]
-        weights=[]
-
-        ol=b[b.filename==fil]['file_label'][i]
-        for lb in list(a):
-            if((a[lb][i]) == 0):
-                weights.append((fil, ol, lb,  (1-a[lb][i])))
-            else:
-                weights.append((fil, ol, lb, (1-a[lb][i])))
-
-
-        all_weights.extend(weights)
-
-    
-        i+=1
-
-
-    X = pd.DataFrame(all_weights).rename(columns={0:'Filename', 1:'Filelabel', 2: 'AllLabels', 3:'Distance_score'})
-
-
-#     for file in list(set(X.Filename)):
-    df = X[X.Filename == file].sort_values(by=['Distance_score'], ascending = False)[0:10]
-    df= df.sort_values(by=['Distance_score'], ascending = True)
     df['color']='green'
-    plt= df.plot.barh(x='AllLabels', y='Distance_score', figsize=(8,5), fontsize=16, color=list(df['color']))
-    plt.set_xticks(np.arange(0.5,1.1, 0.1))
-    plt.set_xlabel('Similarity', fontsize=15)
-    plt.set_ylabel('original_label', fontsize=15)
+    plt= df.plot.barh(x='search_term', y='score', figsize=(6,4), fontsize=10, color=list(df['color']))
+    plt.set_xlabel('Similarity', fontsize=10)
+    plt.set_ylabel('Ranked labels', fontsize=10)
+    plt.axis(xmin=0.0, xmax=1.01)
 
-    plt.figure.savefig('../outputs/bedembed_output/figures/S2/'+file.split('/')[-1]+'.svg', format = 'svg', bbox_inches='tight', dpi =300)
-
+    plt.figure.savefig('{}/{}_nof{}.svg'.format(output, query.split('/')[-1], num_results), format = 'svg', bbox_inches='tight')
     
     
 def run_scenario3(
@@ -184,4 +97,18 @@ def run_scenario3(
 
     # PLACE SEARCH CODE HERE
     
-    
+    file = query.lower()
+    distance =pd.read_csv(distances)
+    distance.test_file = distance.test_file.str.lower()
+    distance.db_file = distance.db_file.str.lower()
+    distance = distance.drop_duplicates()
+    df = distance[distance.test_file.str.contains(file)].sort_values(by = ['score'], ascending = False)[0:num_results]
+    df = df.sort_values(by=['score'], ascending=True)
+
+    df['color']='green'
+    plt= df.plot.barh(x='db_file', y='score', figsize=(6,4), fontsize=10, color=list(df['color']))
+    plt.set_xlabel('Similarity', fontsize=10)
+    plt.set_ylabel('Files in db', fontsize=10)
+    plt.axis(xmin=0.0, xmax=1.01)
+
+    plt.figure.savefig('{}/{}_nof{}.svg'.format(output, query.split('/')[-1], num_results), format = 'svg', bbox_inches='tight')
