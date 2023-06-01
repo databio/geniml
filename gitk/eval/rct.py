881babfafd9681e6c6c261b992e0d027b1b6e01c
Guangtao Zheng <zhguangt@gmail.com>
2023-06-01 17:28:38 -0400

    code reformatting

--- gitk/eval/rct.py
+++ gitk/eval/rct.py
@@ -15,8 +15,7 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 
 from ..utils import timer_func
-from .utils import (Timer, cosine_distance, genome_distance,
-                    load_genomic_embeddings)
+from .utils import Timer, cosine_distance, genome_distance, load_genomic_embeddings
 
 
 def get_rct_score(
