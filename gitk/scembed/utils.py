db183313495d6ba347521dfacfffcdeaba498824
Nathan LeRoy <NLeRoy917@gmail.com>
2023-06-16 16:14:39 -0400

    revert to gzip again for model loading

--- gitk/scembed/utils.py
+++ gitk/scembed/utils.py
@@ -253,8 +253,9 @@ def load_scembed_model(path: str) -> "SCEmbed":
     :param str path: The path to the model.
     """
     import pickle
+    import gzip
 
-    with open(path, "rb") as f:
+    with gzip.open(path, "rb") as f:
         model = pickle.load(f)
         return model
 
