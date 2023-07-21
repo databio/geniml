bab57031f831296b6ade2a123c8a5bf941479f48
nsheff <nsheff@users.noreply.github.com>
2023-07-21 16:32:37 -0400

    some work on tokenizers

--- gitk/tokenization/hard_tokenization_batch.py
+++ gitk/tokenization/hard_tokenization_batch.py
@@ -6,6 +6,57 @@ import subprocess
 from gitk.region2vec import utils
 
 
+class BEDToolsTokenizer(FileTokenizer):
+    """A tokenizer that uses bedtools to tokenize BED files"""
+
+    def __init__(self, bedtools_path: str, universe_path: str = None):
+        """Initialize a BEDToolsTokenizer
+
+        Args:
+            bedtools_path (str): Path to a bedtools binary.
+            universe_path (str): Path to a universe BED file.
+        """
+        self.bedtools_path = bedtools_path
+        self.universe_path = universe_path
+
+    def tokenize(self, input_globs: list[str], universe_path: str = None) -> RegionSet:
+        """Tokenize a RegionSet using bedtools"""
+
+        universe_path = universe_path or self.universe_path
+
+        # loop through globs and tokenize each file
+        for glob in input_globs:
+            for path in glob.glob(glob)
+                _tokenize_one(path, universe_path)
+
+    def _tokenize_one(self, input_path: str, universe_path: str):
+        output_path = os.path.join(input_path, "tokenized.bed")
+        bedtools_path = self.bedtools_path
+        # bedtools can't actually read from stdin, so we have to use a temporary file...
+
+        # sort_process = subprocess.Popen(shlex.split(f"sort -k1,1V -k2,2n {input_path}"), stdout=subprocess.PIPE)
+        # bedtools_process = subprocess.Popen(
+        #     shlex.split(f"{bedtools_path} intersect -a {universe} -b  -u -f {fraction}"),
+        #     stdin = sort_process.stdout,
+        #     stdout = output_file,
+        # )
+        # bedtools_process.communicate()
+
+        # get a temporary file path using tempfile
+        import templfile
+        with tempfile.NamedTemporaryFile() as temp_path, open(output_path, "w") as output_file:
+            # sort the input file
+            sort_process = subprocess.Popen(shlex.split(f"sort -k1,1V -k2,2n {input_path}"), stdout=temp_path)
+            sort_process.communicate()
+            # tokenize the sorted file
+            bedtools_process = subprocess.Popen(
+                shlex.split(f"{bedtools_path} intersect -a {universe} -b {temp_path} -u -f {fraction}"),
+                stdout=output_file,
+            )
+            bedtools_process.communicate()
+
+
+
 def bedtools_tokenization(
     f: str,
     bedtools_path: str,
@@ -31,7 +82,7 @@ def bedtools_tokenization(
         subprocess.run(shlex.split(f"sort -k1,1V -k2,2n {fname}"), stdout=f_temp)
     with open(target, "w") as f_target:
         subprocess.run(
-            shlex.split(f"{bedtools_path} intersect -a {universe} -b {temp} -u -f {fraction} "),
+            shlex.split(f"{bedtools_path} intersect -a {universe} -b {temp} -u -f {fraction}"),
             stdout=f_target,
         )
     os.remove(temp)
