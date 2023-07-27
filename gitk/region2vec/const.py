df6c7f94fb4e531900278698b1465893cdf2c731
Nathan LeRoy <NLeRoy917@gmail.com>
2023-07-27 17:21:28 -0400

    work on adding regions to tokenizer

--- gitk/region2vec/const.py
+++ gitk/region2vec/const.py
@@ -11,3 +11,6 @@ DEFAULT_EPOCHS = 10
 DEFAULT_INIT_LR = 0.1  # https://github.com/databio/gitk/issues/6#issuecomment-1476273162
 DEFAULT_MIN_LR = 0.0001  # gensim default
 DEFAULT_DECAY_RATE = 0.95
+
+MODEL_FILE_NAME = "model.bin"
+UNIVERSE_FILE_NAME = "universe.bed"
