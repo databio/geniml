bd48aa386ce228f298c9f0c54cf3633f1a20e112
Nathan LeRoy <NLeRoy917@gmail.com>
2023-07-27 11:46:14 -0400

    create new Region2Vec class for use with other models

--- gitk/region2vec/utils.py
+++ gitk/region2vec/utils.py
@@ -3,8 +3,23 @@ import select
 import shutil
 import sys
 import time
-from typing import Union, Dict
+from typing import Union, Dict, List
+from enum import Enum
+from random import shuffle
+import logging
 import numpy as np
+
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
+
+from ..io import RegionSet, Region
+from .const import (
+    MODULE_NAME,
+    DEFAULT_INIT_LR,
+    DEFAULT_MIN_LR,
+)
+
+_LOGGER = logging.getLogger(MODULE_NAME)
 
 
 def prRed(skk: str) -> None:
@@ -252,3 +267,149 @@ def ensure_dir(folder: str, default: str = "y") -> None:
         else:
             return
     os.makedirs(folder, exist_ok=True)
+
+
+def wordify_region(region: Region) -> str:
+    """
+    Convert a region to a word of the form "chr_start_end"
+
+    :param Region region: A region
+
+    :return: A word
+    """
+    return f"{region.chr}_{region.start}_{region.end}"
+
+
+def wordify_regions(regions: Union[RegionSet, List[Region]]) -> List[str]:
+    """
+    Convert a list of regions to a list of words. Each region is converted
+    to a word of the form "chr_start_end"
+
+    :param RegionSet | list[Region] regions: A list of regions
+
+    :return: A list of words
+    """
+    return [f"{r.chr}_{r.start}_{r.end}" for r in regions]
+
+
+class ScheduleType(Enum):
+    """Learning rate schedule types"""
+
+    LINEAR = "linear"
+    EXPONENTIAL = "exponential"
+
+
+class LearningRateScheduler:
+    """
+    Simple class to track learning rates of the training procedure
+
+    Based off of: https://machinelearningmastery.com/using-learning-rate-schedules-deep-learning-models-python-keras/
+    """
+
+    def __init__(
+        self,
+        init_lr: float = DEFAULT_INIT_LR,
+        min_lr: float = DEFAULT_MIN_LR,
+        type: Union[str, ScheduleType] = ScheduleType.EXPONENTIAL,
+        decay: float = None,
+        n_epochs: int = None,
+    ):
+        """
+        :param float init_lr: The initial learning rate
+        :param float min_lr: The minimum learning rate
+        :param str type: The type of learning rate schedule to use. Must be one of ['linear', 'exponential'].
+        :param float decay: The decay rate to use. If None, this will be calculated from init_lr and n_epochs.
+        :param int n_epochs: The number of epochs to train for. Only used if decay is None.
+        """
+        self.init_lr = init_lr
+        self.min_lr = min_lr
+        self.n_epochs = n_epochs
+
+        # convert type to learning rate if necessary
+        if isinstance(type, str):
+            try:
+                self.type = ScheduleType[type.upper()]
+            except KeyError:
+                raise ValueError(
+                    f"Unknown schedule type: {type}. Must be one of ['linear', 'exponential']."
+                )
+
+        # init the current lr and iteration
+        self._current_lr = init_lr
+        self._iter = 1
+
+        # init decay rate
+        if decay is None:
+            _LOGGER.warning(
+                "No decay rate provided. Calculating decay rate from init_lr and n_epochs."
+            )
+            self.decay = init_lr / n_epochs
+        else:
+            self.decay = decay
+
+    def _update_linear(self, epoch: int):
+        """
+        Update the learning rate using a linear schedule.
+
+        :param int epoch: The current epoch
+        """
+
+        lr = self.init_lr - (self.decay * epoch)
+        return max(lr, self.min_lr)
+
+    def _update_exponential(self, epoch: int):
+        """
+        Update the learning rate using an exponential schedule.
+
+        :param int epoch: The current epoch
+        """
+        lr = self.get_lr() * (1 / (1 + self.decay * epoch))
+        return max(lr, self.min_lr)
+
+    def update(self):
+        # update the learning rate according to the type
+        if self.type == ScheduleType.LINEAR:
+            self._current_lr = self._update_linear(self._iter)
+            self._iter += 1
+        elif self.type == ScheduleType.EXPONENTIAL:
+            self._current_lr = self._update_exponential(self._iter)
+            self._iter += 1
+        else:
+            raise ValueError(f"Unknown schedule type: {self.type}")
+
+    def get_lr(self):
+        return self._current_lr
+
+
+def shuffle_documents(
+    documents: List[List[str]],
+    n_shuffles: int,
+    threads: int = None,
+) -> List[List[str]]:
+    """
+    Shuffle around the genomic regions for each cell to generate a "context".
+
+    :param List[List[str]] documents: the document list to shuffle.
+    :param int n_shuffles: The number of shuffles to conduct.
+    :param int threads: The number of threads to use for shuffling.
+    """
+
+    def shuffle_list(l: List[str], n: int) -> List[str]:
+        for _ in range(n):
+            shuffle(l)
+        return l
+
+    _LOGGER.debug(f"Shuffling documents {n_shuffles} times.")
+    shuffled_documents = documents.copy()
+    with ThreadPoolExecutor(max_workers=threads) as executor:
+        shuffled_documents = list(
+            tqdm(
+                executor.map(
+                    shuffle_list,
+                    shuffled_documents,
+                    [n_shuffles] * len(documents),
+                ),
+                total=len(documents),
+            )
+        )
+    return shuffled_documents
