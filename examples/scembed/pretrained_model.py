cbe9bbd11b07748d5ed22469afad5009b91b2705
Nathan LeRoy <NLeRoy917@gmail.com>
2023-08-16 13:58:05 -0400

    tweaks

--- examples/scembed/pretrained_model.py
+++ examples/scembed/pretrained_model.py
@@ -5,11 +5,11 @@ from gitk.scembed import ScEmbed
 
 # %%
 # Load the pretrained model
-model = ScEmbed("databio/r2v-luecken2021-hg38-small")
+model = ScEmbed("databio/r2v-Luecken2021-hg38")
 
 # %%
 # Load the data
-data_path = os.path.expandvars("$DATA/genomics/scembed/pbmc_hg38/pbmc.h5ad")
+data_path = os.path.expandvars("$DATA/genomics/scembed/pbmc_hg38/pbmc_hg38.h5ad")
 adata = sc.read_h5ad(data_path)
 
 # %%
