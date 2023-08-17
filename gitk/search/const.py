5eb93e2d250969a27ddcce4951b8a64697f8ea5e
Ziyang "Claude" Hu <33562602+ClaudeHu@users.noreply.github.com>
2023-08-17 16:51:01 -0400

    Update const.py

--- gitk/search/const.py
+++ gitk/search/const.py
@@ -1,2 +1,18 @@
+from qdrant_client.models import VectorParams, Distance
+
 DEFAULT_QDRANT_HOST = "localhost"
 DEFAULT_QDRANT_PORT = 6333
+
+DEFAULT_COLLECTION = "embeddings"
+
+DEFAULT_CONFIG = VectorParams(size=100, distance=Distance.COSINE)
+
+DEFAULT_INDEX_PATH = "./current_index.bin"
+
+DEFAULT_HNSW_SPACE = "cosine"
+
+DEFAULT_DIM = 100
+
+DEFAULT_EF = 200
+
+DEFAULT_M = 16
