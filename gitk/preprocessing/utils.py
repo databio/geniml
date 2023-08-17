ad35d005f315cca17be9842a32732020fc21d79c
Nathan LeRoy <NLeRoy917@gmail.com>
2023-08-17 17:02:57 -0400

    begin formalizing atac transformer preprocessor

---
+++ gitk/preprocessing/utils.py
@@ -0,0 +1,43 @@
+from tqdm import tqdm
+
+from ..io import Region
+
+
+def wordify_region(region: Region) -> str:
+    """
+    Convert a region to a string.
+    """
+    return f"{region.chr}_{region.start}_{region.end}"
+
+
+def unwordify_region(word: str) -> Region:
+    """
+    Convert a string to a region.
+    """
+    chr, start, end = word.split("_")
+    return Region(chr, int(start), int(end))
+
+
+def make_vocab_from_bed(bed_file: str, vocab_file: str, n_unused: int = 10000):
+    """
+    Create a vocabulary file from a bed file. This includes
+    the special tokens [PAD], [UNK], [CLS], [SEP], and [MASK] as
+    well as unused tokens for new regions that may be encountered
+    during training or pre-training.
+    """
+    with open(bed_file, "r") as f:
+        regions = [line.strip().split("\t") for line in f.readlines()]
+
+    with open(vocab_file, "w") as f:
+        f.write("[PAD]\n")
+        f.write("[UNK]\n")
+        f.write("[CLS]\n")
+        f.write("[SEP]\n")
+        f.write("[MASK]\n")
+        for region in tqdm(regions, desc="Writing regions"):
+            r = Region(region[0], int(region[1]), int(region[2]))
+            f.write(wordify_region(r) + "\n")
+
+        # add unused tokens
+        for i in tqdm(range(n_unused), desc="Writing unused tokens"):
+            f.write(f"[UNUSED{i}]\n")
