e47df6bb6048481fcf4ffe6c424c3cac3d1f4295
Nathan LeRoy <NLeRoy917@gmail.com>
2023-08-19 20:09:39 -0400

    finish masked language modeling code to mask tokens

--- gitk/preprocessing/__init__.py
+++ gitk/preprocessing/__init__.py
@@ -1,2 +1,2 @@
-from .main import RegionIDifier, EncodedRegions
+from .main import RegionIDifier, EncodedRegions, TokenMask
 from .utils import make_vocab_from_bed, wordify_region, unwordify_region
--- gitk/preprocessing/schemas.py
+++ gitk/preprocessing/schemas.py
@@ -9,3 +9,12 @@ class EncodedRegions(BaseModel):
 
     ids: List[int]
     attention_mask: List[int]
+
+
+class TokenMask(BaseModel):
+    """
+    A token mask. Contains the masked ids and the indices of the masked tokens.
+    """
+
+    ids: List[int]
+    indices: List[int]
