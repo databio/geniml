bdf4955c1e60821114c0d66a0580ad8080600459
Nathan LeRoy <NLeRoy917@gmail.com>
2023-08-28 10:19:38 -0400

    unit tests for the pooling functions

--- examples/region2ec/pretrained_model.py
+++ examples/region2ec/pretrained_model.py
@@ -1,18 +1,15 @@
-# %%
 import os
 import scanpy as sc
 from gitk.io import RegionSet
 from gitk.region2vec import Region2VecExModel
 
-# %%
 # Load the pretrained model
 model = Region2VecExModel("databio/r2v-ChIP-atlas-hg38")
 
-# %%
 # Load the data
 regions = RegionSet("/path/to/regions.bed")
 
-# %%
+
 # Embed the data
 embeddings = model.encode(regions)
 
