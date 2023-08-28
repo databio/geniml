37316b30aabc8cf307f758f956d149245c314d51
Nathan LeRoy <NLeRoy917@gmail.com>
2023-08-28 12:17:25 -0400

    more unit tests for SNPs and Maf files

--- gitk/io/const.py
+++ gitk/io/const.py
@@ -5,8 +5,8 @@ MAF_ENTREZ_GENE_ID_COL_NAME = "Entrez_Gene_Id"
 MAF_CENTER_COL_NAME = "Center"
 MAF_NCBI_BUILD_COL_NAME = "NCBI_Build"
 MAF_CHROMOSOME_COL_NAME = "Chromosome"
-MAF_START_COL_NAME = "Start_Position"
-MAF_END_COL_NAME = "End_Position"
+MAF_START_COL_NAME = "Start_position"
+MAF_END_COL_NAME = "End_position"
 MAF_STRAND_COL_NAME = "Strand"
 
 MAF_COLUMN = Literal[
--- gitk/io/utils.py
+++ gitk/io/utils.py
@@ -1,4 +1,5 @@
-from typing import Dict
+from typing import Dict, List, Union
+import gzip
 import os
 
 from .const import *
@@ -15,23 +16,43 @@ def is_gzipped(file: str) -> bool:
     return file_extension == ".gz"
 
 
-def extract_maf_col_positions(file: str) -> Dict[MAF_COLUMN, int]:
+def extract_maf_col_positions(file: str) -> Dict[MAF_COLUMN, Union[int, None]]:
     """
     Extract the column positions of the MAF file.
 
     :param file: path to .maf file
     :return: dictionary of column positions
     """
-    with open(file, "r") as f:
+
+    def get_index_from_header(header: List[str], col_name: str) -> int:
+        """
+        Get the index of a column from a header.
+
+        :param header: list of column names
+        :param col_name: column name
+        :return: index of column
+        """
+        try:
+            return header.index(col_name)
+        except ValueError:
+            return None
+
+    # detect open function
+    open_func = open if not is_gzipped(file) else gzip.open
+    mode = "r" if not is_gzipped(file) else "rt"
+
+    with open_func(file, mode) as f:
         header = f.readline().strip().split(MAF_FILE_DELIM)
         col_positions = {
-            MAF_HUGO_SYMBOL_COL_NAME: header.index(MAF_HUGO_SYMBOL_COL_NAME),
-            MAF_ENTREZ_GENE_ID_COL_NAME: header.index(MAF_ENTREZ_GENE_ID_COL_NAME),
-            MAF_CENTER_COL_NAME: header.index(MAF_CENTER_COL_NAME),
-            MAF_NCBI_BUILD_COL_NAME: header.index(MAF_NCBI_BUILD_COL_NAME),
-            MAF_CHROMOSOME_COL_NAME: header.index(MAF_CHROMOSOME_COL_NAME),
-            MAF_START_COL_NAME: header.index(MAF_START_COL_NAME),
-            MAF_END_COL_NAME: header.index(MAF_END_COL_NAME),
-            MAF_STRAND_COL_NAME: header.index(MAF_STRAND_COL_NAME),
+            MAF_HUGO_SYMBOL_COL_NAME: get_index_from_header(header, MAF_HUGO_SYMBOL_COL_NAME),
+            MAF_ENTREZ_GENE_ID_COL_NAME: get_index_from_header(
+                header, MAF_ENTREZ_GENE_ID_COL_NAME
+            ),
+            MAF_CENTER_COL_NAME: get_index_from_header(header, MAF_CENTER_COL_NAME),
+            MAF_NCBI_BUILD_COL_NAME: get_index_from_header(header, MAF_NCBI_BUILD_COL_NAME),
+            MAF_CHROMOSOME_COL_NAME: get_index_from_header(header, MAF_CHROMOSOME_COL_NAME),
+            MAF_START_COL_NAME: get_index_from_header(header, MAF_START_COL_NAME),
+            MAF_END_COL_NAME: get_index_from_header(header, MAF_END_COL_NAME),
+            MAF_STRAND_COL_NAME: get_index_from_header(header, MAF_STRAND_COL_NAME),
         }
     return col_positions
