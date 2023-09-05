098967d44eff65f702dc29833f0154e736c21f3c
Nathan LeRoy <NLeRoy917@gmail.com>
2023-09-04 20:09:59 -0400

    begin migration to pytorch from gensim

--- geniml/region2vec/const.py
+++ geniml/region2vec/const.py
@@ -11,6 +11,7 @@ DEFAULT_EPOCHS = 10
 DEFAULT_INIT_LR = 0.1  # https://github.com/databio/gitk/issues/6#issuecomment-1476273162
 DEFAULT_MIN_LR = 0.0001  # gensim default
 DEFAULT_DECAY_RATE = 0.95
+DEFAULT_HIDDEN_DIM = 100
 
 MODEL_FILE_NAME = "model.bin"
 UNIVERSE_FILE_NAME = "universe.bed"
