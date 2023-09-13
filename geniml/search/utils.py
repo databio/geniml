1d8fc236c8ca94d29bbf4b302af47c54e305fc6c
nsheff <nsheff@users.noreply.github.com>
2023-09-13 07:52:00 -0400

    change qdrant load signature

--- geniml/search/utils.py
+++ geniml/search/utils.py
@@ -1,8 +1,13 @@
-from typing import Dict, List
+from typing import Dict, List, Union
 
 import numpy as np
 
 
-def verify_load_inputs(embeddings: np.ndarray, labels: List[Dict[str, str]]):
-    if embeddings.shape[0] != len(labels):
-        raise KeyError("The number of embeddings does not match the number of labels")
+def verify_load_inputs(vectors: np.ndarray, ids: List[Union[str, int]], payloads: List[Dict[str, str]]):
+    n_ids = len(ids)
+    n_vectors = vectors.shape[0]
+    n_payloads = len(payloads)
+    if n_ids != n_vectors or n_ids != n_payloads:
+        raise ValueError(
+            "The number of ids ({n_ids}), vectors ({n_vectors}), and payloads ({n_payloads}) must match"
+        )
