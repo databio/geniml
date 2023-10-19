5de1333ef3dca171e25b48bd72b8873056bd84ce
Nathan LeRoy <NLeRoy917@gmail.com>
2023-10-19 13:03:50 -0400

    rename things to ITTokenizer

--- geniml/tokenization/__init__.py
+++ geniml/tokenization/__init__.py
@@ -1,2 +1,2 @@
-from .main import FileTokenizer, Tokenizer, InMemTokenizer, GRangesTokenizer, Gtokenizer
+from .main import FileTokenizer, Tokenizer, InMemTokenizer, GRangesTokenizer, ITTokenizer
 from .main import hard_tokenization_main as hard_tokenization
