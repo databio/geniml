7dd8c72b6554853d0f1265c0f663e8edd90bb870
Khoroshevskyi <sasha99250@gmail.com>
2023-10-20 21:45:53 +0200

    deleted unused code

--- geniml/io/utils.py
+++ geniml/io/utils.py
@@ -1,11 +1,8 @@
 import gzip
 import os
-from typing import Dict, List, Union, Any
-
+from typing import Dict, List, Union
 
 from .const import *
-
-# from .io import BedSet, RegionSet
 
 
 def is_gzipped(file: str) -> bool:
@@ -69,68 +66,3 @@ def read_bedset_file(file_path: str) -> List[str]:
         for line in f:
             bed_identifiers.append(line.strip())
     return bed_identifiers
-
-
-# def compute_bed_identifier(bedfile: Any) -> str:
-#     """
-#     Return bed file identifier. If it is not set, compute one
-#
-#     :param bedfile: RegionSet object (Representation of bed_file)
-#     :return: the identifier of BED file (str)
-#     """
-#     if bedfile.identifier is not None:
-#         return bedfile.identifier
-#     else:
-#         if not bedfile.backed:
-#             # concate column values
-#             chrs = ",".join([region.chr for region in bedfile.regions])
-#             starts = ",".join([str(region.start) for region in bedfile.regions])
-#             ends = ",".join([str(region.end) for region in bedfile.regions])
-#
-#         else:
-#             open_func = open if not is_gzipped(bedfile.path) else gzip.open
-#             mode = "r" if not is_gzipped(bedfile.path) else "rt"
-#             with open_func(bedfile.path, mode) as f:
-#                 # concate column values
-#                 chrs = []
-#                 starts = []
-#                 ends = []
-#                 for row in f:
-#                     chrs.append(row.split("\t")[0])
-#                     starts.append(row.split("\t")[1])
-#                     ends.append(row.split("\t")[2].replace("\n", ""))
-#                 chrs = ",".join(chrs)
-#                 starts = ",".join(starts)
-#                 ends = ",".join(ends)
-#
-#         # hash column values
-#         chr_digest = md5(chrs.encode("utf-8")).hexdigest()
-#         start_digest = md5(starts.encode("utf-8")).hexdigest()
-#         end_digest = md5(ends.encode("utf-8")).hexdigest()
-#         # hash column digests
-#         bed_digest = md5(
-#             ",".join([chr_digest, start_digest, end_digest]).encode("utf-8")
-#         ).hexdigest()
-#
-#         bedfile.identifier = bed_digest
-#
-#         return bedfile.identifier
-
-
-# def compute_bedset_identifier(bedset: Any) -> str:
-#     """
-#     Return the identifier. If it is not set, compute one
-#
-#     :param bedset: BedSet object
-#     :return: the identifier of BED set
-#     """
-#     if bedset.bedset_identifier is not None:
-#         return bedset.bedset_identifier
-#
-#     elif bedset.bedset_identifier is None:
-#         bedfile_ids = []
-#         for bedfile in bedset.region_sets:
-#             bedfile_ids.append(compute_bed_identifier(bedfile))
-#         bedset.bedset_identifier = md5(";".join(sorted(bedfile_ids)).encode("utf-8")).hexdigest()
-#
-#         return bedset.bedset_identifier
