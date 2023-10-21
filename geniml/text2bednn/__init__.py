8fe4645713866899910fe0ff0e2e9b0ceb5d9764
Khoroshevskyi <sasha99250@gmail.com>
2023-10-21 18:51:14 +0200

    lint

--- geniml/text2bednn/__init__.py
+++ geniml/text2bednn/__init__.py
@@ -1,4 +1,9 @@
 from .text2bednn import Text2BEDSearchInterface, Vec2VecFNN
-from .utils import (RegionSetInfo, bioGPT_sentence_transformer,
-                    build_regionset_info_list, prepare_vectors_for_database,
-                    region_info_list_to_vectors, vectors_from_backend)
+from .utils import (
+    RegionSetInfo,
+    bioGPT_sentence_transformer,
+    build_regionset_info_list,
+    prepare_vectors_for_database,
+    region_info_list_to_vectors,
+    vectors_from_backend,
+)
