1eced0075922a053250bd2fb4af32d0b83e9b06b
nsheff <nsheff@users.noreply.github.com>
2023-11-09 08:59:31 -0500

    make hnsw optional.  See #107

--- geniml/search/backends/__init__.py
+++ geniml/search/backends/__init__.py
@@ -1,8 +1,2 @@
 from .dbbackend import QdrantBackend
-from ... import _LOGGER
-
-try:
-    from .filebackend import HNSWBackend
-except ImportError:
-    _LOGGER.warning("HNSW backend not available due to missing dependencies.")
-    pass
+from .filebackend import HNSWBackend
