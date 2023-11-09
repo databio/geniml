c026ae1a8b5f9f26c1d5fcba147febaaf7a185c9
nsheff <nsheff@users.noreply.github.com>
2023-11-08 22:09:00 -0500

    lint

--- geniml/text2bednn/__init__.py
+++ geniml/text2bednn/__init__.py
@@ -1,7 +1,10 @@
 from .text2bednn import Text2BEDSearchInterface, Vec2VecFNN
-from .utils import (RegionSetInfo,  # bioGPT_sentence_transformer,
-                    arrays_to_torch_dataloader,
-                    build_regionset_info_list_from_files,
-                    build_regionset_info_list_from_PEP,
-                    prepare_vectors_for_database, region_info_list_to_vectors,
-                    vectors_from_backend)
+from .utils import (
+    RegionSetInfo,  # bioGPT_sentence_transformer,
+    arrays_to_torch_dataloader,
+    build_regionset_info_list_from_files,
+    build_regionset_info_list_from_PEP,
+    prepare_vectors_for_database,
+    region_info_list_to_vectors,
+    vectors_from_backend,
+)
