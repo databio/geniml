bcb11d0bf527eba844526615230d22845d2ca5d3
Nathan LeRoy <NLeRoy917@gmail.com>
2024-01-27 17:34:07 -0500

    work on atacformer, mlm adapter, exmodel, and dataset

--- geniml/atacformer/const.py
+++ geniml/atacformer/const.py
@@ -1,6 +1,17 @@
-DEFAULT_HIDDEN_SIZE = 768
+from typing import Literal
+
+DEFAULT_EMBEDDING_DIM = 768
 
 MASK_RATE = 0.15
 REPLACE_WITH_MASK_RATE = 0.8
 REPLACE_WITH_RANDOM_RATE = 0.1
 KEEP_RATE = 0.1
+
+CONFIG_FILE_NAME = "config.yaml"
+MODEL_FILE_NAME = "checkpoint.pt"
+UNIVERSE_FILE_NAME = "universe.bed"
+
+POOLING_TYPES = Literal["mean", "max"]
+POOLING_METHOD_KEY = "pooling_method"
+EMBEDDING_DIM_KEY = "embedding_dim"
+VOCAB_SIZE_KEY = "vocab_size"
--- geniml/training/adapters.py
+++ geniml/training/adapters.py
@@ -6,6 +6,7 @@ import lightning as L
 
 from ..region2vec import Region2VecExModel
 from ..scembed import ScEmbed
+from ..atacformer import AtacformerExModel
 
 
 class CellTypeFineTuneAdapter(L.LightningModule):
@@ -92,3 +93,54 @@ class CellTypeFineTuneAdapter(L.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
+
+
+class MLMAdapter(L.LightningModule):
+    """
+    An adapter for training Atacformer on masked language modeling.
+    """
+
+    def __init__(self, model: AtacformerExModel, **kwargs):
+        """
+        Instantiate a fine-tuning trainer.
+
+        :param Atacformer model: The model to fine-tune.
+
+        :param kwargs: Additional arguments to pass to the LightningModule constructor.
+        """
+        super().__init__(**kwargs)
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.linear = nn.Linear(model._model.config.hidden_size, model._model.config.vocab_size)
+        self.nn_model = model
+        self.tokenizer = model.tokenizer
+
+    def forward(self, x):
+        return self.nn_model(x)
+
+    def training_step(
+        self, batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor], batch_idx: int
+    ):
+        """
+        Perform a training step.
+
+        The batch is a tuple of (tokens, masked_tokens, mask_ids). This step performs
+        masked language modeling as described in the original BERT paper (https://arxiv.org/abs/1810.04805).
+
+        :param batch: The batch
+        :param batch_idx: The batch index
+
+        """
+
+        # move the batch to the device
+        tokens, masked_tokens, mask_ids = batch
+
+        # forward pass for the batch
+        output = self.nn_model(masked_tokens)
+
+        targets = tokens[mask_ids]
+        predictions = output[mask_ids]
+
+        # compute the loss
+        loss = self.loss_fn(predictions, targets)
+        self.log("train_loss", loss)
+        return loss
