83950ff862a0e505ab88c3eecdad66935032d3ca
Nathan LeRoy <NLeRoy917@gmail.com>
2024-01-29 20:32:09 -0500

    work on the AtacformerExModel

--- geniml/atacformer/main.py
+++ geniml/atacformer/main.py
@@ -1,13 +1,20 @@
+import os
 from typing import Union, List
 
 import torch
 import torch.nn as nn
+from huggingface_hub import hf_hub_download
+from yaml import safe_dump, safe_load
 
 from ..models.main import ExModel
 from ..tokenization.main import ITTokenizer
 from .const import (
     POOLING_TYPES,
     POOLING_METHOD_KEY,
+    D_MODEL_KEY,
+    VOCAB_SIZE_KEY,
+    NUM_LAYERS_KEY,
+    NHEAD_KEY,
     DEFAULT_EMBEDDING_DIM,
     CONFIG_FILE_NAME,
     MODEL_FILE_NAME,
@@ -16,7 +23,13 @@ from .const import (
 
 
 class Atacformer(nn.Module):
-    def __init__(self, vocab_size, d_model: int = DEFAULT_EMBEDDING_DIM):
+    def __init__(
+        self,
+        vocab_size: int,
+        d_model: int = DEFAULT_EMBEDDING_DIM,
+        nhead: int = 8,
+        num_layers: int = 6,
+    ):
         """
         Atacformer is a transformer-based model for ATAC-seq data. It closely follows
         the architecture of BERT, but with a few modifications:
@@ -24,9 +37,12 @@ class Atacformer(nn.Module):
         - no next sentence prediction task
         """
         super().__init__()
+        self.d_model = d_model
+        self.nhead = nhead
+        self.num_layers = num_layers
         self.embedding = nn.Embedding(vocab_size, d_model)
-        self.encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=8)
-        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=6)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead)
+        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=num_layers)
 
     def forward(self, x: torch.Tensor):
         """
@@ -107,9 +123,11 @@ class AtacformerExModel(ExModel):
         :param kwargs: Additional keyword arguments to pass to the model.
         """
         self._init_tokenizer(tokenizer)
-        self._model = Region2Vec(
+        self._model = Atacformer(
             len(self.tokenizer),
-            embedding_dim=kwargs.get("embedding_dim", DEFAULT_EMBEDDING_DIM),
+            d_model=kwargs.get("d_model", DEFAULT_EMBEDDING_DIM),
+            nhead=kwargs.get("nhead", 8),
+            num_layers=kwargs.get("num_layers", 6),
         )
 
     @property
@@ -126,11 +144,40 @@ class AtacformerExModel(ExModel):
         :param str model_path: Path to the model checkpoint.
         :param str vocab_path: Path to the vocabulary file.
         """
-        _model, tokenizer, config = load_local_region2vec_model(
-            model_path, vocab_path, config_path
+        # init the tokenizer - only one option for now
+        self.tokenizer = ITTokenizer(vocab_path)
+
+        # load the model state dict (weights)
+        params = torch.load(model_path)
+
+        # get the model config (vocab size, embedding size)
+        with open(config_path, "r") as f:
+            config = safe_load(f)
+
+        # try with new key first, then old key for backwards compatibility
+        d_model = config.get(D_MODEL_KEY, None)
+        if d_model is None:
+            raise KeyError(
+                f"Could not find embedding dimension in config file. Expected key {D_MODEL_KEY}."
+            )
+
+        nhead = config.get(NHEAD_KEY, None)
+        if nhead is None:
+            raise KeyError(f"Could not find nhead in config file. Expected key {NHEAD_KEY}.")
+
+        num_layers = config.get(NUM_LAYERS_KEY, None)
+        if num_layers is None:
+            raise KeyError(
+                f"Could not find num_layers in config file. Expected key {NUM_LAYERS_KEY}."
+            )
+
+        vocab_size = config.get(VOCAB_SIZE_KEY) or len(self.tokenizer)
+
+        model = Atacformer(
+            vocab_size=vocab_size, d_model=d_model, nhead=nhead, num_layers=num_layers
         )
-        self._model = _model
-        self.tokenizer = tokenizer
+        model.load_state_dict(params)
+
         self.trained = True
         if POOLING_METHOD_KEY in config:
             self.pooling_method = config[POOLING_METHOD_KEY]
@@ -190,6 +237,7 @@ class AtacformerExModel(ExModel):
         checkpoint_file: str = MODEL_FILE_NAME,
         universe_file: str = UNIVERSE_FILE_NAME,
         config_file: str = CONFIG_FILE_NAME,
+        **kwargs,
     ):
         """
         Function to facilitate exporting the model in a way that can
@@ -198,19 +246,48 @@ class AtacformerExModel(ExModel):
 
         :param str path: Path to export the model to.
         """
+        # make sure the path exists
+        if not os.path.exists(path):
+            os.makedirs(path)
 
-        export_region2vec_model(
-            self._model,
-            self.tokenizer,
-            path,
-            checkpoint_file=checkpoint_file,
-            universe_file=universe_file,
-            config_file=config_file,
-        )
+        # make sure the model is unwrapped
+        if isinstance(self._model, nn.DataParallel):
+            self._model = self._model.module
+        elif isinstance(self._model, nn.parallel.DistributedDataParallel):
+            self._model = self._model.module
+        elif isinstance(self._model, nn.Module):
+            pass
+        else:
+            raise TypeError("model must be an nn.Module object.")
 
-    def encode(
-        self, regions: Union[Region, List[Region]], pooling: POOLING_TYPES = None
-    ) -> np.ndarray:
+        # export the model weights
+        torch.save(self._model.state_dict(), os.path.join(path, checkpoint_file))
+
+        # export the vocabulary
+        with open(os.path.join(path, universe_file), "a") as f:
+            for region in self.tokenizer.universe.regions:
+                f.write(f"{region.chr}\t{region.start}\t{region.end}\n")
+
+        d_model = self._model.d_model
+        num_layers = self._model.num_layers
+        nhead = self._model.nhead
+        vocab_size = len(self.tokenizer)
+
+        config = {
+            POOLING_METHOD_KEY: self.pooling_method,
+            D_MODEL_KEY: d_model,
+            VOCAB_SIZE_KEY: vocab_size,
+            NUM_LAYERS_KEY: num_layers,
+            NHEAD_KEY: nhead,
+        }
+
+        if kwargs:
+            config.update(kwargs)
+
+        with open(os.path.join(path, config_file), "w") as f:
+            safe_dump(config, f)
+
+    def encode(self):
         """
         Get the vector for a region.
 
@@ -219,35 +296,4 @@ class AtacformerExModel(ExModel):
 
         :return np.ndarray: Vector for the region.
         """
-        # allow for overriding the pooling method
-        pooling = pooling or self.pooling_method
-
-        # data validation
-        if isinstance(regions, Region):
-            regions = [regions]
-        if isinstance(regions, str):
-            regions = list(RegionSet(regions))
-        if isinstance(regions, RegionSet):
-            regions = list(regions)
-        if not isinstance(regions, list):
-            regions = [regions]
-        if not isinstance(regions[0], Region):
-            raise TypeError("regions must be a list of Region objects.")
-
-        if pooling not in ["mean", "max"]:
-            raise ValueError(f"pooling must be one of {POOLING_TYPES}")
-
-        # tokenize the region
-        tokens = [self.tokenizer.tokenize(r) for r in regions]
-        tokens = [t.id for sublist in tokens for t in sublist]
-
-        # get the vector
-        region_embeddings = self._model(torch.tensor(tokens))
-
-        if pooling == "mean":
-            return torch.mean(region_embeddings, axis=0).detach().numpy()
-        elif pooling == "max":
-            return torch.max(region_embeddings, axis=0).values.detach().numpy()
-        else:
-            # this should be unreachable
-            raise ValueError(f"pooling must be one of {POOLING_TYPES}")
+        # TODO: write this function
