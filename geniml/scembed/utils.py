5703fa0df133a62457b1e4142f52c89dfe115afe
Nathan LeRoy <NLeRoy917@gmail.com>
2024-02-17 19:38:52 -0500

    add batche correction dataset

--- geniml/scembed/utils.py
+++ geniml/scembed/utils.py
@@ -1,4 +1,12 @@
+import os
+from glob import glob
+from typing import Tuple, List
+
 import scanpy as sc
+
+import torch
+from torch.utils.data import Dataset
+from genimtools.utils import read_tokens_from_gtok
 
 from .const import DEFAULT_CHUNK_SIZE
 
@@ -36,3 +44,38 @@ class AnnDataChunker:
 
     def __repr__(self):
         return f"<AnnDataChunker: {self.n_chunks} chunks of size {self.chunk_size}>"
+
+
+class BatchCorrectionDataset(Dataset):
+    def __init__(self, batches: list):
+        """
+        Dataset for batch correction. This dataset takes in pre-tokenized
+        cells and their batch of origin and then yields them out for training.
+
+        :param batches list: a list of paths that point to pre-tokenized cells (.gtok files).
+        """
+        self.num_batches = len(batches)
+
+        # create tuples of (gtok_file, batch)
+        self.data: List[Tuple[str, int]] = []
+        for i, batch in enumerate(batches):
+            for gtok_file in glob(os.path.join(batch, "*.gtok")):
+                self.data.append((gtok_file, i))
+
+    def __getitem__(self, idx) -> Tuple:
+        """
+        Get a single item from the dataset.
+
+        :param idx: The index of the item to get.
+        """
+        gtok_file, batch = self.data[idx]
+        tokens = read_tokens_from_gtok(gtok_file)
+        return tokens, batch
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        return (
+            f"<BatchCorrectionDataset: {len(self.data)} samples, and {self.num_batches} batches>"
+        )
