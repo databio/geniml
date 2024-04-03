238e808f572677fa09bebaf3d85f05fe4d2a9b2d
Khoroshevskyi <sasha99250@gmail.com>
2024-04-03 22:18:35 +0200

    fixed bbclient

--- geniml/bbclient/const.py
+++ geniml/bbclient/const.py
@@ -8,8 +8,8 @@ DEFAULT_BEDFILE_SUBFOLDER = "bedfiles"
 DEFAULT_BEDSET_EXT = ".txt"
 DEFAULT_BEDFILE_EXT = ".bed.gz"
 
-BEDSET_URL_PATTERN = "{bedbase_api}/bedset/{bedset_id}/bedfiles"
-BEDFILE_URL_PATTERN = "{bedbase_api}/objects/bed.{bed_id}.bedfile/access/http/bytes"
+BEDSET_URL_PATTERN = "{bedbase_api}/v1/bedset/{bedset_id}/bedfiles"
+BEDFILE_URL_PATTERN = "{bedbase_api}/v1/objects/bed.{bed_id}.bed_file/access/http/bytes"
 
 BBCLIENT_CACHE_ENV = "BBCLIENT_CACHE"
 
