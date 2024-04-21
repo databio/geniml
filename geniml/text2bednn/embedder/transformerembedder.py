1fde0e8a451944085e3d384f5de6c5c07c6b2bce
Ziyang "Claude" Hu <33562602+ClaudeHu@users.noreply.github.com>
2024-04-21 16:01:46 -0400

    Add files via upload

---
+++ geniml/text2bednn/embedder/transformerembedder.py
@@ -0,0 +1,45 @@
+import logging
+
+import numpy as np
+import torch
+
+from ..const import BIO_BERT_REPO, MODULE_NAME
+from .abstract import TextEmbedder
+
+_LOGGER = logging.getLogger(MODULE_NAME)
+
+try:
+    from transformers import AutoModel, AutoTokenizer
+
+    DEP_TRANSFORMERS = True
+except ImportError:
+    DEP_TRANSFORMERS = False
+    _LOGGER.error("Install transformers, or ignore this if you don't need it")
+
+
+if not DEP_TRANSFORMERS:
+
+    class BioBertEmbedder(TextEmbedder):
+        pass
+
+else:
+
+    class TransformerEmbedder(TextEmbedder):
+        """sentence-transformers pulling biogpt model"""
+
+        def __init__(self, model_name: str = BIO_BERT_REPO):
+            """
+            from https://github.com/UKPLab/sentence-transformers/issues/1824
+            """
+            self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+            self.model = AutoModel.from_pretrained("dmis-lab/biobert-v1.1")
+
+        def forward(self, input_text: str) -> np.ndarray:
+            encoded_input = self.tokenizer(input_text, return_tensors="pt")
+
+            with torch.no_grad():
+                model_output = self.model(**encoded_input)
+
+            embeddings = model_output.last_hidden_state
+
+            return embeddings.mean(dim=1)
