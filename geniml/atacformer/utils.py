ecfeb47d201259bfa2003e265be3ed415caa4e1f
Nathan LeRoy <NLeRoy917@gmail.com>
2024-05-29 15:36:48 -0400

    add extensive documentation to masking strategy

--- geniml/atacformer/utils.py
+++ geniml/atacformer/utils.py
@@ -17,7 +17,7 @@ class AtacformerMLMDataset(Dataset):
         data: str,
         mask_token_id: int,
         vocab_size: int,
-        mask_prob: float = MASK_RATE,
+        mask_rate: float = MASK_RATE,
         random_seed: int = 42,
         shuffle: bool = True,
     ):
@@ -27,13 +27,13 @@ class AtacformerMLMDataset(Dataset):
 
         :param str data: Path to the dataset. This should be a file of .gtok files
         :param int mask_token_id: ID of the mask token
-        :param float mask_prob: Probability of masking a token
+        :param float mask_rate: Probability of masking a token
         :param int vocab_size: Size of the vocabulary
         :param int random_seed: Random seed to use
         :param bool shuffle: Whether to shuffle the data
         """
         self.data = data
-        self.mask_prob = mask_prob
+        self.mask_rate = mask_rate
         self.mask_token_id = mask_token_id
         self.vocab_size = vocab_size
         self.random_seed = random_seed
@@ -50,22 +50,39 @@ class AtacformerMLMDataset(Dataset):
         """
         This should return a tuple of (tokens, masked_tokens, mask_ids).
         """
-        # load the data
+        # load the data into memory
         tokens = torch.tensor(read_tokens_from_gtok(self.files[idx]))
         masked_tokens = tokens.clone()
 
-        # get the mask ids (select tokens.shape[0] * self.mask_prob tokens to mask)
+        # select the tokens to mask
+        #   -- each token has an **equal probability** of being masked ( i.e. torch.ones(tokens.shape[0]))
+        #   -- we sample a certain percentage of tokens to mask (in BERT this is 15%) ( i.e. ceil(tokens.shape[0] * self.mask_rate) )
+        #   -- a token can't be masked more than once (replacement=False)
         mask_ids = torch.multinomial(
-            torch.ones(tokens.shape[0]), ceil(tokens.shape[0] * 0.15), replacement=False
+            torch.ones(tokens.shape[0]), ceil(tokens.shape[0] * self.mask_rate), replacement=False
         )
 
-        # mask the tokens
+        # perform the actual masking. there are three possible outcomes:
+        #   1. mask the token
+        #   2. replace the token with a random token
+        #   3. keep the token the same as it was
+        # each outcome has a different probability of happening (`REPLACE_WITH_MASK_RATE`, `REPLACE_WITH_RANDOM_RATE`, `KEEP_RATE` represented in `self.probs`)
+        #   therefore, we need a decision for each token selected to be masked. this means we set num_samples = mask_ids.shape[0] (total number of
+        #   tokens selected to mask) we need to sample with replacement (replacement=True) because the same outcome can occur multiple times. e.g. multiple
+        #   tokens can be replaced with the mask token.
         random_vals = torch.multinomial(self.probs, mask_ids.shape[0], replacement=True)
 
-        # mask the tokens
+        # now actually mask the tokens. recall that we defined the distribution as:
+        #   `[REPLACE_WITH_MASK_RATE, REPLACE_WITH_RANDOM_RATE, KEEP_RATE]`
+        #   so if the random value is:
+        #   - 0, we *mask the token*,
+        #   - 1, we *replace it with a random token*
+        #   - 2, we *keep it the same*
         mask_token_indices = mask_ids[random_vals == 0]
         replace_random_indices = mask_ids[random_vals == 1]
+        # why is there no need to do anything for random_vals == 2? because we're keeping the token the same...
 
+        # perform the masking
         masked_tokens[mask_token_indices] = self.mask_token_id
         masked_tokens[replace_random_indices] = torch.randint(
             self.vocab_size, (replace_random_indices.shape[0],)
