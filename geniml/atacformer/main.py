fix atacformer attention mask issues

geniml/atacformer/main.py
@@ -29,6 +29,7 @@ class Atacformer(nn.Module):
         d_model: int = DEFAULT_EMBEDDING_DIM,
         nhead: int = 8,
         num_layers: int = 6,
+        context_size: int = 2048,
     ):
         """
         Atacformer is a transformer-based model for ATAC-seq data. It closely follows
@@ -41,6 +42,7 @@ class Atacformer(nn.Module):
         self.nhead = nhead
         self.num_layers = num_layers
         self.vocab_size = vocab_size
+        self.context_size = context_size
         self.embedding = nn.Embedding(vocab_size, d_model)
         self.encoder_layer = nn.TransformerEncoderLayer(
             d_model=d_model, nhead=nhead, batch_first=True
@@ -58,12 +60,8 @@ class Atacformer(nn.Module):
         # set the positional embeddings to 0
         x = x + torch.zeros_like(x)
 
-        # get attention mask in the correct shape
-        if mask is not None:
-            mask = mask.unsqueeze(1).unsqueeze(2)
-
         # pass through the transformer
-        x = self.transformer_encoder(x, mask=mask)
+        x = self.transformer_encoder(x, src_key_padding_mask=mask)
         return x
 
 
@@ -140,6 +138,7 @@ class AtacformerExModel(ExModel):
             d_model=kwargs.get("d_model", DEFAULT_EMBEDDING_DIM),
             nhead=kwargs.get("nhead", 8),
             num_layers=kwargs.get("num_layers", 6),
+            context_size=kwargs.get("context_size", 2048),
         )
 
     @property

geniml/atacformer/utils.py
@@ -16,8 +16,9 @@ class AtacformerMLMCollator:
     Collator for the MLM dataset. This will pad the tokens, masked_tokens, and mask_ids
     """
 
-    def __init__(self, padding_token: int):
+    def __init__(self, padding_token: int, context_size: int):
         self.padding_token = padding_token
+        self.context_size = context_size
 
     def __call__(
         self, batch: List[Tuple[torch.Tensor, torch.Tensor, torch.Tensor]]
@@ -38,10 +39,13 @@ class AtacformerMLMCollator:
         )
         mask_ids = pad_sequence(mask_ids, batch_first=True, padding_value=self.padding_token)
 
-        # attention_mask = tokens != self.padding_token
-        # attention mask needs to be of the shape (batch_size, seq_len, seq_len)
-        attention_mask = torch.ones(tokens.shape[0], tokens.shape[1], tokens.shape[1])
-        attention_mask = attention_mask.masked_fill(tokens == self.padding_token, 0)
+        attention_mask = tokens != self.padding_token
+
+        # clip the tokens to the context size
+        tokens = tokens[:, : self.context_size]
+        masked_tokens = masked_tokens[:, : self.context_size]
+        mask_ids = mask_ids[:, : self.context_size]
+        attention_mask = attention_mask[:, : self.context_size]
 
         return tokens, masked_tokens, mask_ids, attention_mask
 