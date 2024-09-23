37671561df9990879989e8c66241dfed1a3db66d
Khoroshevskyi <sasha99250@gmail.com>
2024-09-23 12:16:08 -0400

    Fixed #171

--- geniml/region2vec/__init__.py
+++ geniml/region2vec/__init__.py
@@ -1,3 +1,3 @@
-from .main import Region2Vec, Region2VecExModel
-from .main_legacy import region2vec
-from .pooling import *
+# from .main import Region2Vec, Region2VecExModel
+# from .main_legacy import region2vec
+#
