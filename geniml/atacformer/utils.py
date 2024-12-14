2859882823f62088172bee5e58ab53767e068fec
Nathan LeRoy <NLeRoy917@gmail.com>
2024-12-13 19:53:31 -0500

    fix merge issues

--- geniml/atacformer/utils.py
+++ geniml/atacformer/utils.py
@@ -38,10 +38,7 @@ class AtacformerMLMCollator:
         )
         mask_ids = pad_sequence(mask_ids, batch_first=True, padding_value=self.padding_token)
 
-        # attention_mask = tokens != self.padding_token
-        # attention mask needs to be of the shape (batch_size, seq_len, seq_len)
-        attention_mask = torch.ones(tokens.shape[0], tokens.shape[1], tokens.shape[1])
-        attention_mask = attention_mask.masked_fill(tokens == self.padding_token, 0)
+        attention_mask = (tokens != self.padding_token).float()
 
         return tokens, masked_tokens, mask_ids, attention_mask
 
@@ -55,6 +52,8 @@ class AtacformerMLMDataset(Dataset):
         mask_rate: float = MASK_RATE,
         random_seed: int = 42,
         shuffle: bool = True,
+        context_size: int = 2048,
+        seed: int = 42,
     ):
         """
         Initialize the MLM dataset. This is heavily based on the MLM dataset
@@ -73,9 +72,14 @@ class AtacformerMLMDataset(Dataset):
         self.vocab_size = vocab_size
         self.random_seed = random_seed
         self.shuffle = shuffle
+        self.context_size = context_size
 
         # get list of all files
-        self.files = glob(os.path.join(data, "*.gtok"))
+        self.files = glob(os.path.join(data, "*.gtok"), recursive=True)
+        if len(self.files) == 0:
+            # try recursive
+            self.files = glob(os.path.join(data, "**/*.gtok"), recursive=True)
+
         self.probs = torch.tensor([REPLACE_WITH_MASK_RATE, REPLACE_WITH_RANDOM_RATE, KEEP_RATE])
 
     def __len__(self):
@@ -87,6 +91,17 @@ class AtacformerMLMDataset(Dataset):
         """
         # load the data into memory
         tokens = torch.tensor(read_tokens_from_gtok(self.files[idx]))
+
+        # reduce the tokens to the context size
+        # randomly sample self.context_size tokens from the tokens
+        # but dont just slice it.... actually just
+        # pick self.context_points without replacement
+        if tokens.shape[0] > self.context_size:
+            indices = torch.multinomial(
+                torch.ones(tokens.shape[0]), self.context_size, replacement=False
+            )
+            tokens = tokens[indices]
+
         masked_tokens = tokens.clone()
 
         # select the tokens to mask
@@ -133,3 +148,110 @@ class AtacformerMLMDataset(Dataset):
 
     def __repr__(self):
         return f"AtacformerMLMDataset({len(self)} files)"
+
+
+class AtacformerCellTypeFineTuningCollator:
+    """
+    Collator for the cell type fine-tuning dataset. This will pad the tokens, labels, and attention_mask
+    """
+
+    def __init__(self, padding_token: int):
+        self.padding_token = padding_token
+
+    def __call__(
+        self, batch: List[Tuple[torch.Tensor, torch.Tensor]]
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        Collate function for the cell type fine-tuning dataset. This should take a batch of
+        (tokens, labels) and return a tuple of (tokens, labels) that are padded
+
+        :param list[tuple[torch.Tensor, torch.Tensor]] batch: Batch of (tokens, labels)
+        :param int padding_token: Token to use for padding
+        """
+        cell1, cell2, labels = zip(*batch)
+
+        # pad the tokens
+        cell1 = pad_sequence(cell1, batch_first=True, padding_value=self.padding_token)
+        cell2 = pad_sequence(cell2, batch_first=True, padding_value=self.padding_token)
+
+        attention_mask1 = (cell1 != self.padding_token).float()
+        attention_mask2 = (cell2 != self.padding_token).float()
+
+        labels = torch.tensor(labels)
+
+        return cell1, cell2, labels, attention_mask1, attention_mask2
+
+
+class AtacformerCellTypeFineTuningDataset(Dataset):
+    def __init__(
+        self,
+        file_path: str,
+        context_size: int = 2048,
+        seed: int = 42,
+    ):
+        """
+        Initialize the cell type fine-tuning dataset.
+
+        :param str file_path: Path to the file that defines the dataset
+        """
+        # check file and not directory
+        if os.path.isdir(file_path):
+            raise ValueError(f"Expected a file, got a directory: {file_path}")
+
+        if not os.path.exists(file_path):
+            raise ValueError(f"File does not exist: {file_path}")
+
+        # init params
+        self.file_path = file_path
+        self.root_dir = os.path.dirname(self.file_path)
+        self.context_size = context_size
+        self.seed = seed
+
+        # read the file, line by line
+        # format is a\tb\label
+        self.pairs = []
+        with open(file_path, "r") as f:
+            for line in f:
+                barcode1, barcode2, label, cell_type1, cell_type2 = line.strip().split("\t")
+                barcode1_path = os.path.join(self.root_dir, barcode1)
+                barcode2_path = os.path.join(self.root_dir, barcode2)
+                self.pairs.append((barcode1_path, barcode2_path, int(label)))
+
+        self.max_label = max([pair[2] for pair in self.pairs])
+
+    def __len__(self):
+        return len(self.pairs)
+
+    def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        This should return a tuple of (tokens, label).
+        """
+        barcode1_path, barcode2_path, label = self.pairs[idx]
+
+        # load the data into memory
+        cell1 = torch.tensor(read_tokens_from_gtok(barcode1_path + ".gtok"))
+        cell2 = torch.tensor(read_tokens_from_gtok(barcode2_path + ".gtok"))
+
+        # reduce the tokens to the context size
+        # randomly sample self.context_size tokens from the tokens
+        if cell1.shape[0] > self.context_size:
+            indices = torch.multinomial(
+                torch.ones(cell1.shape[0]), self.context_size, replacement=False
+            )
+            cell1 = cell1[indices]
+
+        if cell2.shape[0] > self.context_size:
+            indices = torch.multinomial(
+                torch.ones(cell2.shape[0]), self.context_size, replacement=False
+            )
+            cell2 = cell2[indices]
+
+        label = torch.tensor(label)
+
+        return cell1, cell2, label
+
+    def __str__(self):
+        return f"AtacformerCellTypeFineTuningDataset({len(self)} files)"
+
+    def __repr__(self):
+        return f"AtacformerCellTypeFineTuningDataset({len(self)} files)"
