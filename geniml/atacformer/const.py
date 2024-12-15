219c460e3ecda9b96e75900b724ccb2b45825c11
Nathan LeRoy <NLeRoy917@gmail.com>
2024-12-15 15:16:23 -0500

    fix merge issues

--- geniml/atacformer/const.py
+++ geniml/atacformer/const.py
@@ -9,6 +9,7 @@ MASK_START = 0
 MASK_END = 0
 
 DEFAULT_EMBEDDING_DIM = 768
+DEFAULT_CONTEXT_SIZE = 2048
 
 MASK_RATE = 0.15
 REPLACE_WITH_MASK_RATE = 0.8
@@ -18,10 +19,13 @@ KEEP_RATE = 0.1
 CONFIG_FILE_NAME = "config.yaml"
 MODEL_FILE_NAME = "checkpoint.pt"
 UNIVERSE_FILE_NAME = "universe.bed"
+UNIVERSE_CONFIG_FILE_NAME = "universe.toml"
 
 POOLING_TYPES = Literal["mean", "max"]
 POOLING_METHOD_KEY = "pooling_method"
-D_MODEL_KEY = "embedding_dim"
+D_MODEL_KEY = "d_model"
 VOCAB_SIZE_KEY = "vocab_size"
-NUM_LAYERS_KEY = "num_layers"
-NHEAD_KEY = "nheads"
+N_HEADS_KEY = "n_heads"
+N_LAYERS_KEY = "n_layers"
+CONTEXT_SIZE_KEY = "context_size"
+D_FF_KEY = "d_ff"
