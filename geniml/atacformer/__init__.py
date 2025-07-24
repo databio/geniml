e6891e7e6982e0a944dd95a37af6e92401c21ba7
Nathan LeRoy <NLeRoy917@gmail.com>
2025-07-24 14:06:00 -0400

    add patch function to atacformer __init__.py

--- geniml/atacformer/__init__.py
+++ geniml/atacformer/__init__.py
@@ -2,9 +2,15 @@ from transformers import AutoConfig
 
 from ._version import VERSION
 from .data_processing import TrainingTokenizer
-from .modeling_atacformer import AtacformerModel, AtacformerForMaskedLM, AtacformerForReplacedTokenDetection, AtacformerForCellClustering, AtacformerForUnsupervisedBatchCorrection
+from .modeling_atacformer import (
+    AtacformerModel,
+    AtacformerForMaskedLM,
+    AtacformerForReplacedTokenDetection,
+    AtacformerForCellClustering,
+    AtacformerForUnsupervisedBatchCorrection,
+)
 from .configuration_atacformer import AtacformerConfig
-from .modeling_utils import freeze_except_last_n
+from .modeling_utils import freeze_except_last_n, patch_atacformer_model_for_mps
 from .training_utils import (
     DataCollatorForReplacedTokenDetection,
     DataCollatorForTripletLoss,
@@ -35,6 +41,7 @@ __all__ = [
     "get_decaying_cosine_with_hard_restarts_schedule_with_warmup",
     "get_git_hash",
     "freeze_except_last_n",
+    "patch_atacformer_model_for_mps",
 ]
 __version__ = VERSION
 __author__ = "Nathan LeRoy"
