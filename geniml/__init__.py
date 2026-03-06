7a96b33f8214b8581c4c9d3144841b9c85812dc9
nsheff <nsheff@users.noreply.github.com>
2026-03-06 17:26:40 -0500

    add comment about heavy dep gating

--- geniml/__init__.py
+++ geniml/__init__.py
@@ -1,3 +1,9 @@
+# Submodules with heavy optional dependencies (torch, scanpy, gensim)
+# are NOT imported here. Import them directly:
+#   from geniml.region2vec import Region2VecExModel
+#   from geniml.scembed import ScEmbed
+# Install ML dependencies with: pip install geniml[ml]
+
 from logging import getLogger
 
 from ._version import __version__
